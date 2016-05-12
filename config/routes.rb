ae9fe7ec1b7a12a30e270d3fdd64bb784cd72901
Mike Jerome <m@j4e.name>
2016-05-12 21:03:35 +0000

    Add views for editing and updating cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :cards
+  resources :cards, except: :show
+  root to: 'cards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
